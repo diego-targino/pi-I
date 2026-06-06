df9f230eabf3e7a9dff6159dd144f09672b8e330
RanielS <ranielflash69@gmail.com>
2026-06-06 10:52:41 -0300

    lista e métodos de recuperação para análise de usuários

--- api/analysis/views.py
+++ api/analysis/views.py
@@ -23,3 +23,31 @@ class PlantAnalysisViewSet(viewsets.ViewSet):
             result,
             status=status.HTTP_200_OK
         )
+        
+def list(self, request): 
+    user_id = request.query_params.get("userId")
+    if not user_id:
+        return Response(
+            {"message": "userId é obrigatório"},
+            status=status.HTTP_400_BAD_REQUEST)
+    result = PlantAnalysisService.get_history(int(user_id)
+    )
+    return Response(
+        result,
+        status=status.HTTP_200_OK
+    )
+
+    def retrieve(self, request, pk=None):
+    user_id = request.query_params.get("userId")
+    if not user_id:
+        return Response(
+            {"message": "userId é obrigatório"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    result = PlantAnalysisService.get_details(int(pk),
+        int(user_id))
+
+    return Response(
+        result,
+        status=status.HTTP_200_OK
+    )
