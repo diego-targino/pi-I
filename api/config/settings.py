b785dbb809e36f6bd74cabe401d55df875be972b
diego-targino <diegotargino190@gmail.com>
2026-06-21 17:33:23 -0300

    feat: update environment configuration to use decouple for SECRET_KEY and DEBUG settings

--- api/config/settings.py
+++ api/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +23,10 @@ MEDIA_ROOT = BASE_DIR / "media"
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2n%(au#98e)8p)%65p2fju234m6imq5$4kc!gzocasdiz%$=ko'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     "pi-i.onrender.com",
@@ -118,8 +119,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-from decouple import config
 
 DATABASES = {
     'default': {
